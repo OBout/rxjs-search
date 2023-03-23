717070c51b07a5072fa15a0979e1e98473e63de4
Oscar Bout <bb@oscoweb.com>
2023-03-23 17:07:06 +0100

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-search-example';
+  searchInput = new FormControl();
+  filteredUsers$: Observable<User[]>;
+  private users: User[] = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Carol' },
+    { id: 4, name: 'David' },
+    { id: 5, name: 'Eve' },
+    { id: 6, name: 'Frank' }
+  ];
+
+  constructor() {
+    this.filteredUsers$ = this.searchInput.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((searchText: string) => this.searchUsers(searchText))
+    );
+  }
+
+  private searchUsers(searchText: string): Observable<User[]> {
+    if (!searchText || searchText.trim().length === 0) {
+      return of(this.users);
+    }
+
+    const lowerCaseSearchText = searchText.toLowerCase();
+    return of(
+      this.users.filter(user =>
+        user.name.toLowerCase().includes(lowerCaseSearchText)
+      )
+    );
+  }
 }
